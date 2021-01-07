76672e8f2ff5e106d8d2eee4c093eb60f3cb3e9b
Anurag Bisht <anuragbisht91@gmail.com>
2021-01-07 13:40:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+      </header>
+      <body style = " background-color: #282c34;">
+      <p>
+        this is the body of the div 
+      </p>
+      </body>
+    </div>
+  );
+}
+
+
+
+
+
+ export default App;
